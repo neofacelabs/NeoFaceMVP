e81784c25c86c06e07f0a24b8a89c7609c8ff94c
DivyeBhatnagar <divyebhatnagar784@gmail.com>
2026-07-01 01:07:36 +0530

    fix: resolve relative path layout import in app/(footer)/security/page.tsx

--- frontend/app/(footer)/security/page.tsx
+++ frontend/app/(footer)/security/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import SubpageLayout from "../../../components/SubpageLayout";
+import SubpageLayout from "../../components/SubpageLayout";
 
 const SectionCard = ({
   title,
